b39e3c705c2549913db25fb2e1b1c13723f11fa5
Christopher Crouch <chrisdc@gmail.com>
2015-01-06 22:36:32 +0000

    Enqueue fonts

--- functions.php
+++ functions.php
@@ -146,6 +146,9 @@ endif;
  * Enqueue scripts and styles.
  */
 function daniela_scripts() {
+	// Add custom fonts, used in the main stylesheet.
+	wp_enqueue_style( 'daniela-fonts', daniela_fonts_url(), array(), null );
+
 	wp_enqueue_style( 'daniela-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'daniela-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
