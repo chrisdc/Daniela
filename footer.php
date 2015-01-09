01d3058e57e412f5e8a58c550fb870f579897012
Christopher Crouch <chrisdc@gmail.com>
2015-01-09 20:51:42 +0000

    Setup social media menu

--- footer.php
+++ footer.php
@@ -11,6 +11,16 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php if ( has_nav_menu( 'social' ) ) : ?>
+			<nav id="social-navigation" class="social-navigation" role="navigation">
+				<?php wp_nav_menu( array(
+	 				'theme_location' => 'social',
+	 				'link_before' => '<span class="screen-reader-text">',
+	 				'link_after' => '</span>'
+				) ); ?>
+			</nav>
+		<?php endif; ?>
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'daniela' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'daniela' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
