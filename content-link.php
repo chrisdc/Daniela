c0ad2ea2510f5a5da8a9636edb8f11dd891c68f9
Christopher Crouch <chrisdc@gmail.com>
2015-01-16 21:26:15 +0000

    Setup post formats

--- content-link.php
+++ content-link.php
@@ -13,11 +13,7 @@
 		<?php endif; ?>
 
 		<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-			endif;
+			the_title( sprintf( '<h1 class="entry-title"><a href="%s">', esc_url( daniela_get_link_url() ) ), '</a></h1>' );
 		?>
 	</header><!-- .entry-header -->
 
--- functions.php
+++ functions.php
@@ -67,7 +67,7 @@ function daniela_setup() {
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
 	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
+		'aside', 'image', 'video', 'quote', 'link', 'gallery'
 	) );
 
 	// Set up the WordPress core custom background feature.
