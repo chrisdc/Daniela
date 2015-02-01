9d24cc2653d76c0a8768a915d4a2c52edbc4a077
Christopher Crouch <chrisdc@gmail.com>
2015-02-01 21:36:36 +0000

    get template part by post format

--- single.php
+++ single.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'single' ); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
 
 			<?php daniela_post_nav(); ?>
 
