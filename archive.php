1def1650732f5a2861d211b5e97544a629e342d1
Christopher Crouch <chrisdc@gmail.com>
2015-02-24 23:32:30 +0000

    Switch to simple archive title

--- archive.php
+++ archive.php
@@ -8,19 +8,14 @@
  */
 
 get_header(); ?>
+	<header class="page-header">
+		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+	</header><!-- .page-header -->
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
