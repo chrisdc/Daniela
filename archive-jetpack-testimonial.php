8fa731170892bab849f84bf4c5c79a8c2f5f91dd
Christopher Crouch <chrisdc@gmail.com>
2015-03-08 23:18:29 +0000

    Switch to archive-title

--- archive-jetpack-testimonial.php
+++ archive-jetpack-testimonial.php
@@ -8,14 +8,8 @@
 get_header(); ?>
 	<?php $jetpack_options = get_theme_mod( 'jetpack_testimonials' ); ?>
 
-	<?php if ( '' != $jetpack_options['featured-image'] ) : ?>
-	<p class="archive-thumbnail">
-		<?php echo wp_get_attachment_image( (int)$jetpack_options['featured-image'], 'daniela-full-width' ); ?>
-	</p><!-- .archive-thumbnail -->
-	<?php endif; ?>
-
-	<header class="page-header">
-		<h1 class="page-title">
+	<header class="archive-header">
+		<h1 class="achive-title">
 			<?php
 				if ( '' != $jetpack_options['page-title'] )
 					echo esc_html( $jetpack_options['page-title'] );
@@ -24,6 +18,12 @@ get_header(); ?>
 			?>
 		</h1>
 	</header><!-- .page-header -->
+
+	<?php if ( '' != $jetpack_options['featured-image'] ) : ?>
+	<p class="archive-thumbnail">
+		<?php echo wp_get_attachment_image( (int)$jetpack_options['featured-image'], 'daniela-full-width' ); ?>
+	</p><!-- .archive-thumbnail -->
+	<?php endif; ?>
 
 	<?php if ( '' != $jetpack_options['page-content'] ) : ?>
 	<div class="entry-content">
--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 	<header class="page-header">
-		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+		<?php the_archive_title( '<h1 class="archive-title">', '</h1>' ); ?>
 	</header><!-- .page-header -->
 
 	<div id="primary" class="content-area">
