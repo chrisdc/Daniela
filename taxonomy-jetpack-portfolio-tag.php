c2c10ea8a0b3c7d837c5a6882b70e28522c2adab
Christopher Crouch <chrisdc@gmail.com>
2015-04-26 22:14:13 +0100

    Add portfolio tag and type templates

---
+++ taxonomy-jetpack-portfolio-tag.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The archive template for Jetpack portfolio posts.
+ *
+ * @package Daniela
+ */
+
+get_header(); ?>
+	<header class="archive-header">
+		<?php the_archive_title( '<h1 class="archive-title">', '</h1>' ); ?>
+	</header><!-- .page-header -->
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main portfolio-projects" role="main">
+
+		<?php if ( have_posts() ) : ?>
+			<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'portfolio' ); ?>
+
+				<?php endwhile; ?>
+
+				<?php the_posts_navigation( array(
+					'prev_text'          => __( 'Older projects' ),
+					'next_text'          => __( 'Newer projects' ),
+					'screen_reader_text' => __( 'Projects navigation' ),
+				) ); ?>
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
