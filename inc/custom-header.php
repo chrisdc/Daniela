2491fffacdaf4adf4aaf79362d0259085d484b3a
Christopher Crouch <chrisdc@gmail.com>
2015-04-30 18:57:14 +0100

    Finalise custome header settings

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -25,10 +25,11 @@
 function daniela_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'daniela_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-text-color'     => '333',
+		'width'                  => 1020,
+		'height'                 => 300,
 		'flex-height'            => true,
+		'flex-width'             => true,
 		'wp-head-callback'       => 'daniela_header_style',
 		'admin-head-callback'    => 'daniela_admin_header_style',
 		'admin-preview-callback' => 'daniela_admin_header_image',
@@ -44,13 +45,11 @@ if ( ! function_exists( 'daniela_header_style' ) ) :
  */
 function daniela_header_style() {
 	$header_text_color = get_header_textcolor();
-
 	// If no custom options for text are set, let's bail
 	// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value
 	if ( HEADER_TEXTCOLOR == $header_text_color ) {
 		return;
 	}
-
 	// If we get this far, we have custom styles. Let's do this.
 	?>
 	<style type="text/css">
@@ -67,8 +66,7 @@ function daniela_header_style() {
 		// If the user has set a custom color for the text use that
 		else :
 	?>
-		.site-title a,
-		.site-description {
+		.site-title a {
 			color: #<?php echo esc_attr( $header_text_color ); ?>;
 		}
 	<?php endif; ?>
