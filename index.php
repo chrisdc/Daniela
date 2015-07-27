f9fda5b65f6280cdb0a0e3ae855f522d69f0de4a
Christopher Crouch <chrisdc@gmail.com>
2015-07-27 22:28:47 +0100

    Avoid duplicating .page-title in index.php

--- index.php
+++ index.php
@@ -18,14 +18,14 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php if ( is_home() && ! is_front_page() ) : ?>
-					<header>
-						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-					</header>
-				<?php endif; ?>
 
 				<?php
 					/* Include the Post-Format-specific template for the content.
